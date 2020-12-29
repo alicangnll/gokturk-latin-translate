39a910448d2750a21d6f7cff0a0cdf6725d68306
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-12-29 22:47:35 +0300

    Update gokturk_translate.php

--- gokturk_translate.php
+++ gokturk_translate.php
@@ -1,8 +1,70 @@
 <?php
-//Writer : ALİ CAN GONULLU
-// 2020.07.10
 $get = strtoupper($_POST["text"]);
-$proca = str_replace("A", "𐰁", $get);
+if(empty($get)) {
+die('  <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-colors.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-rtl.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-icons.min.css">
+<script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+<style>
+container {width: 100%;}
+
+@media (min-width: 576px) {
+    .container {
+        max-width: 540px;
+    }
+}
+
+@media (min-width: 768px) {
+    .container {
+        max-width: 720px;
+    }
+}
+
+@media (min-width: 992px) {
+    .container {
+        max-width: 960px;
+    }
+}
+
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1140px;
+    }
+}
+
+@media (min-width: 1452px) {
+    .container {
+        max-width: 1360px;
+    }
+}
+</style>
+
+<script>
+function kopyalaButon() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  document.execCommand("copy");
+  alert("Copied the text!");
+}
+</script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="text-center container mt-5">
+<textarea id="myInput" data-role="textarea">Boş karakter girdiniz!</textarea>
+<br>
+<button class="button primary" onclick="kopyalaButon()">Copy text</button>
+</div>');
+} else {
+$yeniproc1 = str_replace("ü", "U", $get);
+$yeniproc2 = str_replace("Ğ", "G", $yeniproc1);
+$yeniproc3 = str_replace("ş", "Ş", $yeniproc2);
+$yeniproc4 = str_replace("ı", "I", $yeniproc3);
+$yeniproc5 = str_replace("ö", "Ö", $yeniproc4);
+$yeniproc6 = str_replace("ğ", "G", $yeniproc5);
+$yeniproc7 = str_replace("ç", "Ç", $yeniproc6);
+$proca = str_replace("A", "𐰁", $yeniproc7);
 $procb = str_replace("B", "𐰉", $proca);
 $procc = str_replace("C", "𐰲", $procb);
 $procc2 = str_replace("Ç", "𐰲", $procc);
@@ -32,5 +94,60 @@ $procv = str_replace("V", "𐰋", $procu2);
 $procy = str_replace("Y", "𐰘", $procv);
 $procz = str_replace("Z", "𐰔", $procy);
 $procbosluk = str_replace(" ", ":", $procz);
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">'.$procbosluk.'';
+echo '  <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-colors.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-rtl.min.css">
+<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-icons.min.css">
+<script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+<style>
+container {width: 100%;}
+
+@media (min-width: 576px) {
+    .container {
+        max-width: 540px;
+    }
+}
+
+@media (min-width: 768px) {
+    .container {
+        max-width: 720px;
+    }
+}
+
+@media (min-width: 992px) {
+    .container {
+        max-width: 960px;
+    }
+}
+
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1140px;
+    }
+}
+
+@media (min-width: 1452px) {
+    .container {
+        max-width: 1360px;
+    }
+}
+</style>
+
+<script>
+function kopyalaButon() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  document.execCommand("copy");
+  alert("Copied the text!");
+}
+</script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="text-center container mt-5">
+<textarea id="myInput" data-role="textarea">'.strip_tags($procbosluk).'</textarea>
+<br>
+<button class="button primary" onclick="kopyalaButon()">Copy text</button>
+</div>';
+}
 ?>
